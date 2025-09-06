b4838f8da840e3a3c16aac8b5299cb485ea85c55
trumpolin <RobertSambale@gmx.de>
2025-09-06 21:08:07 +0200

    Allow long-running sync to run asynchronously

--- modules/growset/src/Controller/Admin/SyncProductsController.php
+++ modules/growset/src/Controller/Admin/SyncProductsController.php
@@ -3,6 +3,7 @@
 namespace Growset\Controller\Admin;
 
 use ModuleAdminController;
+use Symfony\Component\Process\Process;
 
 class SyncProductsController extends ModuleAdminController
 {
@@ -10,15 +11,27 @@ class SyncProductsController extends ModuleAdminController
     {
         parent::initContent();
         $this->executeSync();
-        $this->content = '<p>Sync triggered.</p>';
-        $this->context->smarty->assign('content', $this->content);
+        if (empty($this->errors)) {
+            $this->content = '<p>Sync triggered.</p>';
+            $this->context->smarty->assign('content', $this->content);
+        }
     }
 
     protected function executeSync(): void
     {
-        $cmd = _PS_ROOT_DIR_ . '/bin/console growset:sync';
-        if (function_exists('exec')) {
-            @exec('php ' . escapeshellcmd($cmd) . ' > /dev/null 2>&1 &');
+        $console = _PS_ROOT_DIR_ . '/bin/console';
+        $process = new Process(['php', $console, 'growset:sync']);
+        $process->setTimeout(null);
+        $process->disableOutput();
+
+        try {
+            $process->start();
+        } catch (\Throwable $e) {
+            $message = sprintf('Sync command failed to start: %s', $e->getMessage());
+            $this->errors[] = $message;
+            if (class_exists('\\PrestaShopLogger')) {
+                \PrestaShopLogger::addLog($message, 3);
+            }
         }
     }
 }
