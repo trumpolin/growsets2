b645154f7b85b9a15b6255c1cd6ba83d9c320427
trumpolin <RobertSambale@gmx.de>
2025-09-06 17:11:27 +0200

    fix: remove deprecated next export script

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+};
