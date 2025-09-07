7141c05bbc0bd93d55c081ff619f97c39572282f
trumpolin <RobertSambale@gmx.de>
2025-09-07 20:50:34 +0200

    Add frontend build assets and display controller

---
+++ modules/growset2/assets/_next/static/chunks/1447098c236a44c3.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/015d003db409fdc5.js",
+  "static/chunks/turbopack-925ddd923d69f769.js"
+])
---
+++ modules/growset2/assets/_next/static/chunks/84e622c40e2d7a03.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/015d003db409fdc5.js",
+  "static/chunks/turbopack-274ac1ae09a780ad.js"
+])
