1768c5b4b4750fd8a943fec0ffac8136ff7b0288
trumpolin <RobertSambale@gmx.de>
2025-09-07 22:22:47 +0200

    chore(frontend): scope asset prefix to production

---
+++ modules/growset2/controllers/front/display.php
@@ -0,0 +1,9 @@
+<?php
+class Growset2DisplayModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
+        parent::initContent();
+        Tools::redirect($this->module->getPathUri() . 'assets/index.html', null);
+    }
+}
