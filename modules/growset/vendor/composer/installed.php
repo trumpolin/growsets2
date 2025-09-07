a469d6078dc61fb76b3bd003c7fca8f289f3fcd6
trumpolin <RobertSambale@gmx.de>
2025-09-07 02:05:01 +0200

    Include Composer autoload for Growset module

---
+++ modules/growset/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'growset/module',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8dc51dbfd97b606b19f11de8e7a41f9d5b72179d',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'growset/module' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8dc51dbfd97b606b19f11de8e7a41f9d5b72179d',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
